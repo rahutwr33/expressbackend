82a64b41fb0dbcd4436cc6ba46bd1f8ec9d75eca
rahul <rahultwr33@gmail.com>
2021-05-26 16:46:21 +0530

    latest

--- controller/product.js
+++ controller/product.js
@@ -1,15 +1,21 @@
-const Product = require('../model/product')
 const mongoose = require('mongoose');
 mongoose.set('useFindAndModify', false);
-
+const Product = require('../model/product')
 const {connectDatabase} = require('../config/database')
+const elasticsearch = require("elasticsearch");
+const esClient = elasticsearch.Client({
+  host: "http://127.0.0.1:9200",
+})
+
 const saveProduct = async function(req,res){
     const dbcon = await connectDatabase(); 
     const newUser = new Product(req.body);
     newUser.save().then(() => {
-        console.log('data save');
-        dbcon.connection.close() //close connection
-        return res.status(200).json({"success": true, message: "product created"})
+        Product.on('es-indexed', function(err, res){
+            if (err) throw err;
+            dbcon.connection.close() //close connection
+            return res.status(200).json({"success": true, message: "product created"})
+        })
     }).catch(err => {
         dbcon.connection.close() //close connection
         return res.status(500).json({"success": false, message: err.toString()})
@@ -23,14 +29,36 @@ const uploadImage = async function(req,res){
 
 const getProduct = async function(req,res){
     const dbcon = await connectDatabase();  //connect to database common method
-    Product.find({},function(err,product){
-        if(err){
-            return res.status(401).json({"success": false, message: err.toString()});
-        }else{
-            dbcon.connection.close() //close connection 
-            return res.status(200).json({"success": true, data: product})
+    const searchText = req.query.search;
+    esClient.search({
+        index: "products",
+        body: {
+            query: {
+                match: {"name": searchText.trim()},
+            },
+            "aggs": {
+                "docs": {
+                  "top_hits": {
+                    "size": 1
+                  }
+                }
+              }
         }
     })
+    .then(response => {
+        return res.json(response)
+    })
+    .catch(err => {
+        return res.status(500).json({"message": "Error"})
+    })
+    // Product.find({},function(err,product){
+    //     if(err){
+    //         return res.status(401).json({"success": false, message: err.toString()});
+    //     }else{
+    //         dbcon.connection.close() //close connection 
+    //         return res.status(200).json({"success": true, data: product})
+    //     }
+    // })
 }
 
 
@@ -38,7 +66,7 @@ const updateProduct = async function(req,res){
     const dbcon = await connectDatabase();  //connect to database common method
     Product.findByIdAndUpdate(mongoose.Types.ObjectId(String(req.params.id)), req.body, function(err,product){
         if(err){
-            return res.status(401).json({"success": false, message: err.toString()});
+            return res.status(500).json({"success": false, message: err.toString()});
         }else{
             dbcon.connection.close() //close connection 
             return res.status(200).json({"success": true, data: product})
@@ -50,10 +78,13 @@ const deleteProduct = async function(req,res){
     const dbcon = await connectDatabase();  //connect to database common method
     Product.findByIdAndRemove(mongoose.Types.ObjectId(req.params.id), function(err,product){
         if(err){
-            return res.status(401).json({"success": false, message: err.toString()});
+            return res.status(500).json({"success": false, message: err.toString()});
         }else{
-            dbcon.connection.close() //close connection 
-            return res.status(200).json({"success": true, message: 'product deleted'})
+            Product.on('es-removed', function(err, res) {
+                if (err) throw err;
+                dbcon.connection.close() //close connection 
+                return res.status(200).json({"success": true, message: 'product deleted'})
+              });
         }
     })
 }
--- model/product.js
+++ model/product.js
@@ -1,21 +1,87 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const mongooseProfiler = require('mongoose-profiler');
-
+const mongoosastic = require('mongoosastic')
 const ProductSchema = new Schema({
-  name: {type: String, required: true, unique: true},
-  price: Number,
-  quantity:   Number,
+  name: { type: String, required: true, unique: true, es_indexed: true },
+  price: { type: Number },
+  quantity: Number,
   image: String,
-  category: String,
+  category: { type: String, es_indexed: true },
   isdeleted: { type: Boolean, default: false },
-},{ timestamps: true},
-{autoIndex:true});
+}, { timestamps: true },
+  { autoIndex: true });
 
-ProductSchema.plugin(mongooseProfiler({
-  isAlwaysShowQuery: true,
-  duration: 1000,          // Show query plans when it took more than this time (ms).
-  totalDocsExamined: 1000, // Show query plans when "totalDocsExamined" more than this value.
-  level: 'COLLSCAN'        // Show query plans when the stage is "COLLSCAN".
-}));
-module.exports = mongoose.model('product', ProductSchema);
+ProductSchema.plugin(mongoosastic, {
+  hosts: [
+    'localhost:9200'
+  ]
+});
+// ProductSchema.plugin(mongooseProfiler({
+//   isAlwaysShowQuery: true,
+//   duration: 1000,          // Show query plans when it took more than this time (ms).
+//   totalDocsExamined: 1000, // Show query plans when "totalDocsExamined" more than this value.
+//   level: 'COLLSCAN'        // Show query plans when the stage is "COLLSCAN".
+// }));
+var Product =  mongoose.model('product', ProductSchema);
+Product.createMapping({
+  "settings": {
+    "analysis": {
+      "analyzer": {
+        "my_analyzer": {
+          "type": "custom",
+          "tokenizer": "classic",
+          "char_filter": [ "my_pattern" ],
+          "filter": ["lowercase"]
+         }
+      },
+      "char_filter": {
+        "my_pattern": {
+          "type": "pattern_replace",
+          "pattern": "\\.",
+          "replacement": " "
+        }
+      }
+    }
+   },
+   "mappings": {
+     "product": {
+       "dynamic_templates": [{
+         "strings": {
+           "match_mapping_type": "string",
+           "mapping": {
+             "type": "text",
+             "fields": {
+               "keyword": {
+                 "type": "keyword"
+               }
+             }
+           }
+         }
+      }],
+      "properties": {
+        "title": {
+          "type": "text",
+          "analyzer": "my_analyzer"
+      },
+      "category": {
+        "type": "keyword"
+      }
+    }
+   },
+  }
+ }, (err, mapping) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(mapping);
+  }
+});
+
+var stream = Product.synchronize();
+
+stream.on('error', function (err) {
+  console.log(`err`, err)
+})
+
+module.exports = Product
